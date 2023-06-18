572a7c279e1b989b8426e5bc906e84f5862a86e9
Nux <eccenux+git@gmail.com>
2023-06-18 22:06:15 +0200

    public chrome config

---
+++ chrome.config.js
@@ -0,0 +1,10 @@
+/**
+ * Chrome configuration.
+ * 
+ * You should have a Canary edition of Chrome. It seems easiest to setup doesn't involve your main Chrome.
+ * 
+ * Then just run Chrome Canary with param: "...\chrome.exe" --remote-debugging-port=9222
+ */
+
+/** Remote debugging port. */
+export const wsBrowserPort = '9222';
