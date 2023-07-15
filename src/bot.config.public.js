259a7afd7bbf6cc0c9a491c025ff6b2d642bcd5a
eccenux <eccenux+git@gmail.com>
2023-07-15 23:20:44 +0200

    v1.4 deploy with bot-API. * bot config example

---
+++ src/bot.config.public.js
@@ -0,0 +1,13 @@
+/**
+	Bot with edit&create rights.
+	
+	You can create the bot account on any wiki. E.g. on the test wiki:
+	https://test.wikipedia.org/wiki/Special:BotPasswords
+
+	The username will be something like `MyName@Wikiploy` where `Wikiploy` would be a bot name
+	(you can choose any bot name but "Wikiploy" would be a good choice to separate it from other things).
+	
+	The password will be something like `12345abcdefpqrst123456abcdef`.
+ */
+export const username = '...@...';
+export const password = '...';
